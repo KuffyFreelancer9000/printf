839a586199d67f300cdbf21245fb503109734e50
KuffyFreelancer9000 <KuffyFreelancer9000@gmai.com>
2022-10-18 21:10:57 -0700

    Creating _printf.c

---
+++ _printf.c
@@ -0,0 +1,77 @@
+#tdlib.h>
+/**
+ *check_for_specifiers-checksifthereisavalidformatspecifier
+ *@format:possibleformatspecifier
+ *
+ *Return:pointertovalidfunctionorNULL
+ */
+staticint(*check_for_specifiers(constchar*format))(va_list)
+{
+unsignedinti;
+print_tp[]={
+{"c",print_c},
+{"s",print_s},
+{"i",print_i},
+{"d",print_d},
+{"u",print_u},
+{"b",print_b},
+{"o",print_o},
+{"x",print_x},
+{"X",print_X},
+{"p",print_p},
+{"S",print_S},
+{"r",print_r},
+{"R",print_R},
+{NULL,NULL}
+};
+for(i=0;p[i].t!=NULL;i++)
+{
+if(*(p[i].t)==*format)
+{
+break;
+}
+}
+return(p[i].f);
+}
+/**
+ *_printf-printsanything
+ *@format:listofargumenttypespassedtothefunction
+ *
+ *Return:numberofcharactersprinted
+ */
+int_printf(constchar*format,...)
+{
+unsignedinti=0,count=0;
+va_listvalist;
+int(*f)(va_list);
+if(format==NULL)
+return(-1);
+va_start(valist,format);
+while(format[i])
+{
+for(;format[i]!='%'&&format[i];i++)
+{
+_putchar(format[i]);
+count++;
+}
+if(!format[i])
+return(count);
+f=check_for_specifiers(&format[i+1]);
+if(f!=NULL)
+{
+count+=f(valist);
+i+=2;
+continue;
+}
+if(!format[i+1])
+return(-1);
+_putchar(format[i]);
+count++;
+if(format[i+1]=='%')
+i+=2;
+else
+i++;
+}
+va_end(valist);
+return(count);
+}
